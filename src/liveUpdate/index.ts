f184765317cb1bb31d066cb72bc92f0aa3691cd2
Phi Henry Nguyen <phi.ng1114@gmail.com>
2020-10-17 12:29:43 -0400

    updated live server

--- src/liveUpdate/index.ts
+++ src/liveUpdate/index.ts
@@ -6,18 +6,27 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/update', (req, res) => {
+	res.sendFile(__dirname + '/update.html');
+});
+
 io.on('connection', (socket) => {
 	console.log('a user connected');
 	socket.on('disconnect', () => {
 		console.log('user disconnected');
 	});
-});
-
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-      console.log('message: ' + msg);
-    });
+	socket.on('chat message', (msg) => {
+		io.emit('chat message', msg);
+		console.log('message: ' + msg);
+	});
+	socket.on('change image', (url, size) => {
+		io.emit('change image', url, size);
+		console.log(url);
+	})
+	socket.on('change video', (url) => {
+		io.emit('change video', url);
+		console.log(url);
+	})
 });
 
 
